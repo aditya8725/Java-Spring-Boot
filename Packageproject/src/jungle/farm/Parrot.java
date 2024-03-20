bcfe2d76edb1c1cd31af0026b46118392ec0e2b3
Aditya Ranjan Pradhan <99034778+aditya8725@users.noreply.github.com>
2024-03-20 15:15:02 +0530

    Add files via upload

---
+++ Packageproject/src/jungle/farm/Parrot.java
@@ -0,0 +1,10 @@
+package jungle.farm;
+
+public class Parrot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Packageproject/src/jungle/farm/Sparrow.java
@@ -0,0 +1,10 @@
+package jungle.farm;
+
+public class Sparrow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
